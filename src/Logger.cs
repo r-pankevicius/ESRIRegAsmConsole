429e4697ba469f4f1d0452139247e6f0851f152a
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2022-01-07 23:10:17 +0200

    Applied KISS rule

--- src/Logger.cs
+++ src/Logger.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ESRIRegAsmConsole
 {
@@ -11,27 +7,31 @@ namespace ESRIRegAsmConsole
 	/// </summary>
 	internal static class Logger
 	{
+		private const ConsoleColor SuccessColor = ConsoleColor.Green;
+		private const ConsoleColor WarningColor = ConsoleColor.Yellow;
+		private const ConsoleColor ErrorColor = ConsoleColor.Red;
+
 		public static void Success(string message)
 		{
-			using var _ = new SuccessForeColor();
-			Console.WriteLine(message);
-		}
-
-		public static void Error(string message)
-		{
-			using var _ = new ErrorForeColor();
+			using var _ = new ForeColor(SuccessColor);
 			Console.WriteLine(message);
 		}
 
 		public static void Warning(string message)
 		{
-			using var _ = new WarningForeColor();
+			using var _ = new ForeColor(WarningColor);
+			Console.WriteLine(message);
+		}
+
+		public static void Error(string message)
+		{
+			using var _ = new ForeColor(ErrorColor);
 			Console.WriteLine(message);
 		}
 
 		#region Private classes
 
-		private abstract class ForeColor : IDisposable
+		private class ForeColor : IDisposable
 		{
 			private readonly ConsoleColor m_PreviousForegroundColor;
 
@@ -42,39 +42,6 @@ namespace ESRIRegAsmConsole
 			}
 
 			public void Dispose() => Console.ForegroundColor = m_PreviousForegroundColor;
-		}
-
-		private class SuccessForeColor : ForeColor
-		{
-			public SuccessForeColor() : base(ConsoleColor.Green) { }
-
-			public static void WriteLine(string line)
-			{
-				using (new ErrorForeColor())
-					Console.WriteLine(line);
-			}
-		}
-
-		private class ErrorForeColor : ForeColor
-		{
-			public ErrorForeColor() : base(ConsoleColor.Red) { }
-
-			public static void WriteLine(string line)
-			{
-				using (new ErrorForeColor())
-					Console.WriteLine(line);
-			}
-		}
-
-		private class WarningForeColor : ForeColor
-		{
-			public WarningForeColor() : base(ConsoleColor.Yellow) { }
-
-			public static void WriteLine(string line)
-			{
-				using (new WarningForeColor())
-					Console.WriteLine(line);
-			}
 		}
 
 		#endregion
